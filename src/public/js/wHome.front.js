f4e9354726aabe6bb3ac7d26b0b595f6531c12e0
farhan1999 <farhanfahim79@gmail.com>
2022-02-25 17:01:48 +0600

    white file upload handler added

--- src/public/js/wHome.front.js
+++ src/public/js/wHome.front.js
@@ -78,17 +78,77 @@ formSubmitAction = function (event){
           else {
               alert('Entry Unsuccessful');
           }
-          //window.location.href = '/white/entry';
+          window.location.href = '/white/entry';
        }
     });
 };
 
+fileUploadHandler = function (event){
+    event.preventDefault();
+    $.ajax({
+        // Your server script to process the upload
+        url: `/white/entry/process_csv/${$('#selectCsv option:selected').val()}`,
+        type: 'POST',
+
+        // Form data
+        data: new FormData($('#csvFileUploadId')[0]),
+
+        // Tell jQuery not to process data or worry about content-type
+        // You *must* include these options!
+        cache: false,
+        contentType: false,
+        processData: false,
+
+        // Custom XMLHttpRequest
+        xhr: function () {
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) {
+                // For handling the progress of the upload
+                myXhr.upload.addEventListener('progress', function (e) {
+                    if (e.lengthComputable) {
+                        $('progress').attr({
+                            value: e.loaded,
+                            max: e.total,
+                        });
+                    }
+                }, false);
+            }
+            return myXhr;
+        },
+        success : function (ret){
+            if(ret.success===true){
+                alert('File uploaded successfully');
+            }
+            else {
+                alert('couldn\'t upload file due to format error');
+            }
+            window.location.href = '/white/entry';
+        },
+        error:function (ret)
+        {
+            alert('Error sending request')
+        }
+    });
+
+};
+
+activeCsv = function (event){
+    if($('#selectCsv option:selected' ).val() !== 'none'){
+        $('#csvSubmitBtn').prop('disabled', false);
+    }
+    else $('#csvSubmitBtn').prop('disabled', true);
+}
+
 $('document').ready(function (){
-    console.log('Document is ready.......');
+
     var subCategoryTemplate = $('#subCategoryT').html();
     showSubCategory = Handlebars.compile(subCategoryTemplate);
 
     $('#regType').on('change',regAction);
     $('#selectCategoryId').on('change',categoryAction);
     $('#submitBtn').on('click',formSubmitAction);
+
+    $('#selectCsv').on('change',activeCsv);
+    $('#csvSubmitBtn').on('click',fileUploadHandler);
+
 });
